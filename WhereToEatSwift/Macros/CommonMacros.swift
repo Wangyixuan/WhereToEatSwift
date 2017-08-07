5601eb0b37ab70e6dbbe5a349d088c1123d3566f
wanglei <605455102@qq.com>
2017-08-07 20:19:24 +0800

    添加图片 基本架构

---
+++ WhereToEatSwift/Macros/CommonMacros.swift
@@ -0,0 +1,13 @@
+//
+//  CommonMacros.swift
+//  WhereToEatSwift
+//
+//  Created by 王磊 on 2017/8/7.
+//  Copyright © 2017年 com.WLTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let appColor = UIColor.init(colorLiteralRed: 255/255.0, green: 128/255.0, blue: 26/255.0, alpha: 1)
+		
--- WhereToEatSwift/ViewController.swift
+++ WhereToEatSwift/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let lab = UILabel()
+        lab.text = "whereToEat-Swift"
+        lab.frame = CGRect(x:100,y:100,width:50,height:20);
+        lab.sizeToFit()
+        self.view.addSubview(lab)
     }
 
     override func didReceiveMemoryWarning() {
